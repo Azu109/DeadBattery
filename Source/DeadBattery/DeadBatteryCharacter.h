068a8e465c7f65ddf0766433381da9d20b277112
Samin Shahriar Tokey <saminshahriar@rocketmail.com>
2023-04-06 20:23:06 -0400

    Revert "Camera Lag Added, Player Rotation Bug Fixed, RILerp Added to aiming, Blood Drain over time added"
    This reverts commit 41cfeea3451b8983c82487464d751eb39043a91a.

--- Source/DeadBattery/DeadBatteryCharacter.h
+++ Source/DeadBattery/DeadBatteryCharacter.h
@@ -71,17 +71,9 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Weapon)
 	float EnergyDrainRate; // Unit per second
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Weapon)
-	float BloodDrainRate; 
-	
-
 	float FireRateTimer;
 
-	float DeltaTime;
-
 	bool CanFire;
-
-	FRotator AimRotation;
 
 	/*UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Component")
 	class USceneComponent* Root;

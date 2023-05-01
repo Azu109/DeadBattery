b0413b163319a6802d9bd8e55591380a6240d8a9
Samin Shahriar Tokey <saminshahriar@rocketmail.com>
2023-05-01 09:04:00 -0400

    Melee nerf

--- Source/DeadBattery/DeadBatteryCharacter.cpp
+++ Source/DeadBattery/DeadBatteryCharacter.cpp
@@ -301,9 +301,9 @@ void ADeadBatteryCharacter::Shoot(const FInputActionValue& Value)
 		CanFire = false;
 	}
 	
-	if (!IsAiming && MeleeCooldownTimer <= 0 && !ShieldSpawned && CurrentEnergyMeter >= 2 * EnergyDrainPerShot)
+	if (!IsAiming && MeleeCooldownTimer <= 0 && !ShieldSpawned && CurrentEnergyMeter >= 3 * EnergyDrainPerShot)
 	{
-		CurrentEnergyMeter -= 2 * EnergyDrainPerShot;
+		CurrentEnergyMeter -= 3 * EnergyDrainPerShot;
 		APlayerController* PlayerController = Cast<APlayerController>(GetController());
 		FHitResult Hit;
 		PlayerController->GetHitResultUnderCursor(ECC_Visibility, false, Hit);
